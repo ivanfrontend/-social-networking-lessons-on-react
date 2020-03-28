1430aed478fc6deff3a060147bd8583d702c6d5b
ivanfrontend <ivanfrond111950@mail.ru>
2020-03-28 13:33:30 +0300

    lesson 51-ok

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import s from './users.module.css';
+import * as axios from 'axios';
+import avatar_base from '../../assets/img/avatar_base.png'
 let Users = (props) => {
-
+        
     if(props.users.length  === 0){
-        props.setUsers([{id: 1, photoUrl: 'https:/img/img.png', followed: false, fullName: 'Ivan V', status: 'test statut', location: {city: 'Minsk', country: 'Belarus1'} },
-        {id: 2, photoUrl: 'https:/img/img.png', followed: false, fullName: 'Dmitri V', status: 'test statut', location: {city: 'Minsk', country: 'Belarus2'} },
-        {id: 3, photoUrl: 'https:/img/img.png', followed: true, fullName: 'Marina V', status: 'test statut', location: {city: 'Minsk', country: 'Belarus3'} },
-        {id: 4, photoUrl: 'https:/img/img.png', followed: true, fullName: 'Sveta V', status: 'test statut', location: {city: 'Minsk', country: 'Belarus4'} }])
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then( response => {
+            // debugger
+            props.setUsers(response.data.items)
+        } )
+
+        // props.setUsers([{id: 1, photoUrl: 'https:/img/img.png', followed: false, fullName: 'Ivan V', status: 'test statut', location: {city: 'Minsk', country: 'Belarus1'} },
+        // {id: 2, photoUrl: 'https:/img/img.png', followed: false, fullName: 'Dmitri V', status: 'test statut', location: {city: 'Minsk', country: 'Belarus2'} },
+        // {id: 3, photoUrl: 'https:/img/img.png', followed: true, fullName: 'Marina V', status: 'test statut', location: {city: 'Minsk', country: 'Belarus3'} },
+        // {id: 4, photoUrl: 'https:/img/img.png', followed: true, fullName: 'Sveta V', status: 'test statut', location: {city: 'Minsk', country: 'Belarus4'} }])
     }
 
    
@@ -14,16 +22,16 @@ let Users = (props) => {
         {
             props.users.map( u => <div className={s.wrap_flex} key={u.id}>
                 <div className={s.photo_and_button}>
-                   <div><img src={u.photoUrl} alt="ava"/></div> 
-                    <div>
+                   <div><img src={u.photos.small ? u.photos.small : avatar_base } alt="ava"/></div> 
+                    <div className={s.w_foll}>
                         {u.followed ? <button onClick={ () => props.unfollow(u.id) } >unfollow</button> : <button onClick={ () => props.follow(u.id) } >follow</button>}
                     </div>
                 </div>
                 <div className={s.user_info}>
-                    <div className={s.wi_50}>{u.fullName}</div>
+                    <div className={s.wi_50}>{u.name}</div>
                     <div className={s.wi_50}>
-                         <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                         <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </div>
                     <div className={s.wi_100}>{u.status}</div>
                 </div>
